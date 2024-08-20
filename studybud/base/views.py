c809d7708c1978d4554f892b135e1233d7f7ba2d
Declan Munene <munene.declan1@gmail.com>
2024-08-20 20:48:21 +0300

    Learning Models

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
+from .models import Room
 
-rooms = [
-    {'id':1, 'name':'lets learn python!'},
-    {'id':2, 'name':'lets learn django!'},
-    {'id':3, 'name':'lets learn flask!'},
-]
+# rooms = [
+#    {'id':1, 'name': 'lets learn python!'},
+#    {'id':2, 'name': 'lets learn django!'},
+#    {'id':3, 'name': 'lets learn flask!'},
+# ]
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
-    return render(request, 'base/home.html')
+    return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    return render(request, 'base/room.html')
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
